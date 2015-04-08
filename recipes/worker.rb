a60a328124289dffd2cd7a5439b3bcfc8bc87b16
Scott Macfarlane <scott@gina.alaska.edu>
2015-04-08 14:07:43 -0800

    Remove commented old code

--- recipes/worker.rb
+++ recipes/worker.rb
@@ -1,21 +1,6 @@
-# include_recipe 'runit'
+include_recipe 'runit'
 include_recipe 'sandy::application'
 
-# concurrency = node['sandy']['worker']['concurrency'] || node['cpu']['total']
-#
-# template "#{node['sandy']['install_dir']}/config/sidekiq.yml" do
-#   source "sidekiq.yml.erb"
-#   owner node['sandy']['account']
-#   group node['sandy']['account']
-#   mode 0644
-#   variables({
-#     concurrency: concurrency,
-#     queues: node['sandy']['worker']['queues']
-#   })
-#   notifies :restart, "runit_service[sidekiq]"
-# end
-#
-#Set path information
 directory node['sandy']['worker']['scripts-path'] do
   owner node['sandy']['account']
   group node['sandy']['account']
@@ -48,22 +33,3 @@ service 'sandy-worker-1' do
 end
 
 
-
-# runit_service 'sidekiq' do
-#   log true
-#   default_logger true
-#   env({
-#     "PATH" => "/usr/bin:/bin:#{node['sandy']['worker']['scripts-path']}/bin",
-#     "SVWAIT" => "15",
-#     "SANDY_SCRATCH_PATH" => node['sandy']['secrets']['scratch_path'],
-#     "SANDY_SHARED_PATH" => node['sandy']['secrets']['shared_path'],
-#     "PROCESSING_NUMBER_OF_CPUS" => "#{node['cpu']['total']}"
-#   })
-#   options({
-#     user: node['sandy']['account'],
-#     rubyversion: node['sandy']['ruby']['version'],
-#     app_dir: "#{node['sandy']['install_dir']}",
-#     environment: node['sandy']['environment'],
-#     pidfile: "#{node['sandy']['install_dir']}/tmp/sidekiq.pid"
-#   })
-# end
