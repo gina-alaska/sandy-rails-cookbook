150415a20fc40151220dfcf297379002f69cafdd
Scott Macfarlane <scott@gina.alaska.edu>
2015-04-17 10:24:59 -0800

    Fix config files

--- recipes/worker.rb
+++ recipes/worker.rb
@@ -26,12 +26,31 @@ directory "#{node['sandy']['worker']['home']}/shared/bundle" do
   recursive true
 end
 
+directory "#{node['sandy']['worker']['home']}/shared/config" do
+  owner 'processing'
+  group 'processing'
+  recursive true
+end
+
+confs = data_bag_item(:apps, 'sandy-utils-production')['configs']
+
+confs.each do |f, conf|
+  file "#{node['sandy']['worker']['home']}/shared/config/#{f}" do
+    owner 'processing'
+    group 'processing'
+    mode 0644
+    content conf.to_yaml
+  end
+end
+
 deploy_revision node['sandy']['worker']['home'] do
   repo node['sandy']['worker']['repo']
   revision node['sandy']['worker']['revision']
   user 'processing'
   group 'processing'
   action node['sandy']['worker']['deploy_action'] || 'deploy'
+
+  symlinks(confs.reduce({}){|result, (k,v)| result["config/#{k}"] = "config/#{k}"; result })
 
   before_restart do
     execute 'bundle install' do
@@ -49,7 +68,7 @@ runit_service "sidekiq" do
   default_logger true
   env({
     "RAILS_ENV" => 'production',
-    "PROCESSING_NUMBER_OF_CPUS" => node['cpu']['total']
+    "PROCESSING_NUMBER_OF_CPUS" => "#{node['cpu']['total']}"
     })
 
   subscribes :restart, "deploy_revision[#{node['sandy']['home']}]", :delayed
