8af85fc8b6bfeb1f2f2b8f6bd14fc5b80c151b8f
Scott Macfarlane <scott@gina.alaska.edu>
2015-04-21 14:23:16 -0800

    Symlink ruby so that chpst env can find them

--- recipes/_ruby.rb
+++ recipes/_ruby.rb
@@ -7,3 +7,9 @@ include_recipe 'chruby'
 %w(libxml2 libxml2-devel libxslt libxslt-devel postgresql-libs).each do |pkg|
   package pkg
 end
+
+%w(bundle bundler ruby).each do |rb|
+  link "/usr/bin/#{rb}" do
+    to "/opt/rubies/#{node['sandy']['ruby']['version']}/bin/#{rb}"
+  end
+end
