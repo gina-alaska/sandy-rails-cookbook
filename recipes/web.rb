80c3975792db188c7bfafff1de301419e5d89d93
Scott Macfarlane <scott@gina.alaska.edu>
2015-09-30 15:16:54 -0800

    Add cronjobs for tasks

--- recipes/web.rb
+++ recipes/web.rb
@@ -7,6 +7,19 @@ include_recipe 'postgresql::client'
 include_recipe "sandy::_user"
 include_recipe "sandy::_application"
 
+cron "fetch-upcoming-pass-info" do
+  time :daily
+  command "/bin/bash -l -c 'cd /opt/sandy/embedded/service/sandy && bin/rails runner -e production '\''Facility.queue_upcoming_passes'\'''"
+  environment "PATH=$PATH:/opt/sandy/embedded/bin"
+  user 'processing'
+end
+
+cron 'cleanup-old-passes' do
+  time :hourly
+  command "/bin/bash -l -c 'cd /opt/sandy/embedded/service/sandy && bin/rails runner -e production '\''Pass.cleanup_old_passes'\'''"
+  environment "PATH=$PATH:/opt/sandy/embedded/bin"
+  user 'processing'
+end
 
 directory '/var/run/sandy' do
   owner 'processing'
