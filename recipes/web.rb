67c3fe7cd5df49edd93977bd0640039e96e74db0
Scott Macfarlane <scott@gina.alaska.edu>
2015-10-02 09:08:12 -0800

    Use correct path

--- recipes/web.rb
+++ recipes/web.rb
@@ -10,14 +10,14 @@ include_recipe "sandy::_application"
 
 cron "fetch-upcoming-pass-info" do
   time :daily
-  command "/bin/bash -l -c 'cd /opt/sandy/embedded/service/sandy && bin/rails runner -e production '\''Facility.queue_upcoming_passes'\'''"
+  command "/bin/bash -l -c 'cd /www/sandy/current && bin/rails runner -e production '\''Facility.queue_upcoming_passes'\'''"
   environment({"PATH" => "$PATH:/opt/sandy/embedded/bin"})
   user 'processing'
 end
 
 cron 'cleanup-old-passes' do
   time :hourly
-  command "/bin/bash -l -c 'cd /opt/sandy/embedded/service/sandy && bin/rails runner -e production '\''Pass.cleanup_old_passes'\'''"
+  command "/bin/bash -l -c 'cd /www/sandy/current && bin/rails runner -e production '\''Pass.cleanup_old_passes'\'''"
   environment({"PATH" => "$PATH:/opt/sandy/embedded/bin"})
   user 'processing'
 end
