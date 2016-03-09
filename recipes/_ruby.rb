988e51bef34aad06ae1fa4e28a472427bd698368
Scott Macfarlane <scott@gina.alaska.edu>
2016-03-08 16:32:38 -0900

    Fetch ruby from s3

--- recipes/_ruby.rb
+++ recipes/_ruby.rb
@@ -1,8 +1,17 @@
 include_recipe "yum-gina"
 
-package node['sandy']['ruby']['package']
-include_recipe 'chruby'
+ruby_package = "#{Chef::Config[:file_cache_path]}/gina-ruby-2.2.0-1.x68_64.rpm"
 
+remote_file ruby_package do
+  source "https://s3-us-west-2.amazonaws.com/gina-packages/gina-ruby-2.2.0-1.x86_64.rpm"
+  notifies :run, 'package[install-ruby]', :immediately
+end
+
+package 'install-ruby' do
+  package_name ruby_package
+end
+
+include_recipe 'chruby'
 
 %w(libxml2 libxml2-devel libxslt libxslt-devel postgresql-libs).each do |pkg|
   package pkg
